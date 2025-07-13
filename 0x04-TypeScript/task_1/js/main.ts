b6a8cc38642c170210090a216658a4128006635a
Meekness James <meeknessjames1@gmail.com>
2025-07-13 13:02:17 +0100

    Task 2: Fix build errors and implement Directors interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,12 +1,14 @@
+// Teacher interface
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [key: string]: any; // allows extra properties
+  [key: string]: any;
 }
 
+// Only one teacher3 object
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -16,3 +18,18 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+// Directors interface that extends Teacher
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
